31d6992c49c8718786c2eefaa21badf1c11eac7c
Sanidhya Samadhiya <sanidhyasamadhiya3@gmail.com>
2019-10-12 17:02:45 +0530

    Initializing repo

---
+++ App.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React,{Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Button,
+  FlatList
+} from 'react-native';
+
+
+class App extends Component{
+
+  constructor(props){
+    super();
+
+  }
+
+  render(){
+    return(
+
+      <View>
+
+        <Text> Mumma's Moppet </Text>
+
+      </View>
+
+
+      )
+  }
+
+}
+
+export default App;
